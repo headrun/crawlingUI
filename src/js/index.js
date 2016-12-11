52d9350aeb21640ec64434d222f173fb1c731af0
prrashi <ninja@fundoocode.ninja>
2016-12-12 01:44:36 +0530

    Added header

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,26 @@
 import React    from "react";
 import ReactDOM from "react-dom";
-import App      from "./AppComponent.js";
 import style    from "../less/index.less";
+import Header from "../components/header/index.js";
 
-console.log("wtf");
-ReactDOM.render(<App/>, document.getElementById("react-root"));
+function App (props) {
+
+	return (
+
+    <div className="app">
+			<Header theme="transparent"
+				      text="Crawling UI"
+							url="index.html"/>
+      <div className="content">
+  		  <div className="container-fluid">
+  			  <h1>Hello World!</h1>
+  			</div>
+      </div>
+		</div>
+	);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById("react-root")
+);
