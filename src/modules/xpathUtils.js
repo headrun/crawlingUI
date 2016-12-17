09914637ab01c904be56ef29539f52a5242dbf0a
prrashi <ninja@fundoocode.ninja>
2016-12-17 13:21:30 +0530

    Writing logic to find shortest xpath

--- src/modules/xpathUtils.js
+++ src/modules/xpathUtils.js
@@ -1,4 +1,10 @@
+var _ = require("underscore");
+
 function areSiblingElements (ele1, ele2) {
+
+  /**
+   * given two different elements, find if they are siblings
+   */
 
   var areSiblings = false,
       prevSibling = ele1.previousSibling,
@@ -150,6 +156,147 @@ function getCommonCSSPath (paths) {
   });
 
   return hasCommonPath ? currentCommonPath : null;
+}
+
+function isMatching (path, nodes) {
+
+  /**
+   * Give a patha and NodeList, returns if both sets match
+   *
+   * @param {string} path
+   * @param {NodeList} nodes
+   */
+
+  if (!(nodes instanceof NodeList)) {
+
+    throw new Error("Expected NodeList, got " + typeof nodes);
+  }
+
+  var nodesByPath    = document.querySelectorAll(path),
+      numNodes       = nodes.length,
+      numNodesByPath = nodesByPath.length;
+
+  if (numNodesByPath !== numNodes) {
+
+    return false;
+  }
+
+  var numMatching = 0;
+
+  for (var i = 0, currentNode; i < numNodes; i++) {
+
+    currentNode = nodes.item(i);
+
+    for (var j = 0, currentPathNode; j < numNodesByPath; j++) {
+
+      if (currentNode === nodesByPath.item(j)) {
+
+        numMatching++;
+        break;
+      }
+    }
+  }
+
+  return numNodes === numMatching;
+}
+
+function getAttributesSelector (nodes) {
+
+  var commonArributesHash = {},
+      allAttributes = nodes.item(0).attributes,
+      selector;
+
+  for (var i = 0, name, value;i < allAttributes.length; i++) {
+
+    name = allAttributes[i].name.toLowerCase();
+    value = allAttributes[i].value;
+
+    if (name === "class") {
+
+      commonArributesHash[name] = value.split(/\s+/);
+    } else {
+
+      commonArributesHash[name] = value;
+    }
+  }
+
+  var commonArributesList = Object.keys(commonArributesList);
+
+  for (var i=0, node; i < nodes.length; i++) {
+
+    node = nodes[i];
+
+    for (var j = 0, attrName, attrValue; j < commonArributesList.length; j++) {
+
+      attrName = commonArributesList[j];
+      attrValue = commonArributesHash[attrName];
+
+      var nodeAttrValue, removeAttribute;
+
+      if (!node.hasAttribute(attrName)) {
+
+        removeAttribute = true;
+      } else {
+
+        nodeAttrValue = node.getAttribute(attrName);
+
+        if (attrName === "class") {
+
+          attrValue = _.intersection(attrValue, nodeAttrValue.split(/\s+/));
+
+          removeAttribute = attrValue.length === 0;
+        } else {
+
+          removeAttribute = attrValue !== nodeAttrValue;
+        }
+      }
+
+      if (removeAttribute) {
+
+        delete commonArributesHash[attrName];
+      }
+
+      commonArributesList = Object.keys(commonArributesHash);
+    }
+  }
+
+  for (var attributeName in commonArributesHash) {
+
+    if (attributeName === "class") {
+
+      selector += "." + commonArributesHash[attributeName].join(".");
+    } else {
+
+      selector += "["+ attributeName +"=\""
+                  + commonArributesHash[attributeName] + "\"]";
+    }
+  }
+
+  return selector;
+}
+
+function optimise (path) {
+
+  if (!path)
+    return null;
+
+  var nodes = document.querySelectorAll(path);
+
+  if (nodes.length === 0) {
+
+    return null;
+  }
+
+  var optimisedPath = elements[0].nodeName.toLowerCase();
+
+  if (isMatching(nodes, document.querySelectorAll(optimisedPath))) {
+
+    return optimisedPath;
+  }
+
+  optimisedPath += getAttributesSelector(nodes);
+
+  
 }
 
 module.exports = {
